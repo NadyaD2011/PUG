cbe2a17e20cdc53f34a26ccf6ab2a2d438686e9e
NadyaEvp <t_evp@mail.ru>
2025-03-14 16:18:03 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,21 @@ const path = require("path");
 const PugPlugin = require('pug-plugin');
 
 const mode = process.env.NODE_ENV || "development" ;
+
 const devMode = mode === "development";
+
 const target = devMode ? "web" : "browserslist";
 const devtool = devMode ? "source-map" : undefined;
+
+module.exports = {
+    mode,
+    target,
+    devtool,
+    entry: {
+        index: path.resolve(__dirname, 'index.pug')
+    },
+    output: {
+        path: path.resolve(__dirname, 'progect'),
+        clear: true,
+    }
+}
