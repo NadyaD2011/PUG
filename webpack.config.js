5ca4725f38de2e07493218127bf2c93977858553
NadyaEvp <t_evp@mail.ru>
2025-03-14 16:54:49 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
+const { generatePrimeSync } = require("crypto");
+const { type } = require("os");
 const path = require("path");
 const PugPlugin = require('pug-plugin');
+const loader = require("sass-loader");
+const { Generator } = require("webpack");
 
 const mode = process.env.NODE_ENV || "development" ;
 
@@ -18,5 +22,49 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'progect'),
         clear: true,
-    }
+    },
+    plugins: [
+        new PugPlugin({
+          pretty: devMode,
+          js: {
+            // JS output filename, used if `inline` option is false (defaults)
+            filename: '[name].[contenthash].js',
+            //inline: true, // inlines JS into HTML
+          },
+          css: {
+            // CSS output filename, used if `inline` option is false (defaults)
+            filename: '[name].[contenthash].css',
+            //inline: true, // inlines CSS into HTML
+          },
+        })
+      ],
+      module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                loader: PugPlugin.loader,
+            },
+            {
+                test: /\.(c|sa|sc)ss$/i,
+                use: [
+                    'sass-loader',
+                    'css-loader'
+                ],
+            },
+            {
+                test: /\.(png|jpg|jpeg|ico)$/,
+                type: 'asser/resource',
+                generator: {
+                    filename: 'media/[name].[hash][ext]'
+                },
+            },
+            {
+                test: /\.svg$/i,
+                type: 'asser/resource',
+                generator: {
+                    filename: 'media/[name][ext][query]'
+                },
+            },
+        ],
+    },
 }
